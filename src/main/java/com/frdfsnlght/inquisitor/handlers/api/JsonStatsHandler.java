4d2838ca98da643009a672e0bc97373013cbdb5d
Samistine <samuel@samistine.com>
2015-04-09 11:25:34 -0400

    Remove this for now

--- src/main/java/com/frdfsnlght/inquisitor/handlers/api/JsonStatsHandler.java
+++ src/main/java/com/frdfsnlght/inquisitor/handlers/api/JsonStatsHandler.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2012 frdfsnlght <frdfsnlght@gmail.com>.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.frdfsnlght.inquisitor.handlers.api;
 
 import com.frdfsnlght.inquisitor.DB;
@@ -37,8 +22,7 @@ public final class JsonStatsHandler extends APIHandler {
     @Override
     public void handleRequest(WebRequest req, WebResponse res) throws IOException {
         String playerName = req.getParameter("playerName", null);
-        System.out.println(playerName);
-        System.out.println("did");
+        System.out.println("Requesting stats from " + playerName);
 
         PreparedStatement stmt = null;
         ResultSet rs = null;
