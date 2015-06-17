e8c7a01ce0517e7f30f7841a885bdcbebbe34b41
Samistine <samuel@samistine.com>
2015-06-16 22:44:52 -0400

    Not sure what I did here, maybe just a code reformat

--- src/main/java/com/frdfsnlght/inquisitor/handlers/api/JsonStatsHandler.java
+++ src/main/java/com/frdfsnlght/inquisitor/handlers/api/JsonStatsHandler.java
@@ -1,6 +1,7 @@
 package com.frdfsnlght.inquisitor.handlers.api;
 
 import com.frdfsnlght.inquisitor.DB;
+import com.frdfsnlght.inquisitor.JSON;
 import com.frdfsnlght.inquisitor.TypeMap;
 import com.frdfsnlght.inquisitor.Utils;
 import com.frdfsnlght.inquisitor.webserver.WebRequest;
@@ -11,6 +12,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import org.json.simple.JSONObject;
 
 /**
@@ -43,58 +45,10 @@ public final class JsonStatsHandler extends APIHandler {
             //while (rs.next()) {//Not needed since we only *should* have one row
             if (rs.next() == true) {
                 TypeMap mainMap = new TypeMap();
-
-                mainMap.set("uuid", rs.getString("uuid"));
-                mainMap.set("displayname", rs.getString("displayName"));
-                mainMap.set("uuid", rs.getString("uuid"));
-
-                int online = rs.getInt("online");
-                boolean onlineBool = false;
-                switch (online) {
-                    case 0:
-                        onlineBool = false;
-                        break;
-                    case 1:
-                        onlineBool = true;
-                        break;
-                }
-                mainMap.set("online", onlineBool);
-                mainMap.set("sessiontime", rs.getFloat("sessionTime"));
-                
-                    TypeMap playerLocation = new TypeMap();
-                    playerLocation.set("server", rs.getString("server")); //for those of you using one database for all your servers ^.^
-                    playerLocation.set("world", rs.getString("world"));
-                    playerLocation.set("coords", rs.getString("coords"));   
-                mainMap.set("playerLocation", playerLocation);
-                
-                    TypeMap joinQuits = new TypeMap();
-                    setJoinQuits(joinQuits, rs); //Populate the list
-                mainMap.set("joinquits", joinQuits); //Add this list to the main list
-                
-                    TypeMap playerStatus = new TypeMap();
-                    playerStatus.set("health", rs.getInt("health"));
-                    playerStatus.set("gameMode", rs.getString("gameMode"));
-                
-                    playerStatus.set("level", rs.getInt("level"));
-                    playerStatus.set("exp", rs.getFloat("exp"));
-                
-                    playerStatus.set("remainingAir", rs.getInt("remainingAir"));
-                    playerStatus.set("fireTicks", rs.getInt("fireTicks")); //Not sure what this is?
-                
-                    playerStatus.set("foodLevel", rs.getInt("foodLevel"));
-                    playerStatus.set("exhaustion", rs.getFloat("exhaustion"));
-                    playerStatus.set("saturation", rs.getFloat("saturation"));
-                    playerStatus.set("potionEffects", rs.getObject("potionEffects"));
-                
-                        TypeMap playerStatusInventory = new TypeMap();
-                        playerStatusInventory.set("heldItemSlot", rs.getInt("heldItemSlot"));
-                        playerStatusInventory.set("inventory", rs.getObject("inventory")); //the actual inventory ^.^
-                        playerStatusInventory.set("armor", rs.getObject("armor"));
-                        playerStatusInventory.set("ender", rs.getObject("ender"));
-                    playerStatus.set("inventory", playerStatusInventory);
-                mainMap.set("playerStatus", playerStatus);
-                
-                
+                    mainMap.set("profile", playerProfile(rs));
+                    mainMap.set("playerLocation", playerLocationMap(rs));
+                    mainMap.set("joinquits", playerJoinQuits(rs)); //Add this list to the main list
+                    mainMap.set("playerStatus", playerStatus(rs));
                 json.putAll(mainMap);
                 System.out.println(json);
                 sendSuccess(req, res, json);
@@ -130,11 +84,69 @@ public final class JsonStatsHandler extends APIHandler {
         }
     }*/
 
-    private void setJoinQuits(TypeMap joinQuits, ResultSet rs) throws SQLException {
-        joinQuits.set("joins", rs.getInt("joins"));
-        joinQuits.set("quits", rs.getInt("quits"));
-        joinQuits.set("firstjoin", String.valueOf(rs.getTimestamp("firstJoin")));
-        joinQuits.set("lastjoin", String.valueOf(rs.getTimestamp("lastJoin")));
-        joinQuits.set("lastquit", String.valueOf(rs.getTimestamp("lastQuit")));
+    private TypeMap playerJoinQuits(ResultSet rs) throws SQLException {
+        TypeMap joinQuits = new TypeMap();
+            joinQuits.set("joins", rs.getInt("joins"));
+            joinQuits.set("quits", rs.getInt("quits"));
+            joinQuits.set("firstjoin", String.valueOf(rs.getTimestamp("firstJoin")));
+            joinQuits.set("lastjoin", String.valueOf(rs.getTimestamp("lastJoin")));
+            joinQuits.set("lastquit", String.valueOf(rs.getTimestamp("lastQuit")));
+        return joinQuits;
+    }
+    private TypeMap playerLocationMap(ResultSet rs) throws SQLException{
+        TypeMap playerLocation = new TypeMap();
+            playerLocation.set("server", rs.getString("server")); //for those of you using one database for all your servers ^.^
+            playerLocation.set("world", rs.getString("world"));
+            playerLocation.set("coords", rs.getString("coords"));
+        return playerLocation;
+    }
+    private TypeMap playerInventories(ResultSet rs) throws SQLException {
+        TypeMap playerStatusInventory = new TypeMap();
+            playerStatusInventory.set("heldItemSlot", rs.getInt("heldItemSlot"));
+            playerStatusInventory.set("inventory", JSON.decode(rs.getObject("inventory").toString()));
+            playerStatusInventory.set("armor", JSON.decode(rs.getObject("armor").toString())); //the actual inventory ^.^
+            playerStatusInventory.set("ender", JSON.decode(rs.getObject("ender").toString()));
+        return playerStatusInventory;
+    }
+    
+    private TypeMap playerStatus(ResultSet rs) throws SQLException {
+        TypeMap playerStatus = new TypeMap();
+            playerStatus.set("health", rs.getInt("health"));
+            playerStatus.set("gameMode", rs.getString("gameMode"));
+                
+            playerStatus.set("level", rs.getInt("level"));
+            playerStatus.set("exp", rs.getFloat("exp"));
+                
+            playerStatus.set("remainingAir", rs.getInt("remainingAir"));
+            playerStatus.set("fireTicks", rs.getInt("fireTicks")); //Not sure what this is?
+                
+            playerStatus.set("foodLevel", rs.getInt("foodLevel"));
+            playerStatus.set("exhaustion", rs.getFloat("exhaustion"));
+            playerStatus.set("saturation", rs.getFloat("saturation"));
+            playerStatus.set("potionEffects", rs.getObject("potionEffects"));
+                
+
+            playerStatus.set("inventory", playerInventories(rs));
+        return playerStatus;
+    }
+    private TypeMap playerProfile(ResultSet rs) throws SQLException {
+        TypeMap profile = new TypeMap();
+            profile.set("uuid", rs.getString("uuid"));
+            profile.set("displayname", rs.getString("displayName"));
+            profile.set("groups", JSON.decode(rs.getObject("groups").toString()));
+
+            int online = rs.getInt("online");
+            boolean onlineBool = false;
+            switch (online) {
+                case 0:
+                    onlineBool = false;
+                    break;
+                case 1:
+                onlineBool = true;
+                break;
+            }
+            profile.set("online", onlineBool);
+            profile.set("sessiontime", rs.getFloat("sessionTime"));
+        return profile;
     }
 }
