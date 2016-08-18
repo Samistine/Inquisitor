5444db3fab0964a15ef523c19c30c6eb4ad8e8a1
Samistine <samuel@samistine.com>
2016-08-18 17:09:41 -0400

    Make BedCheck thread safe + Cleanup Code
    Oops...

--- src/main/java/com/frdfsnlght/inquisitor/BedCheck.java
+++ src/main/java/com/frdfsnlght/inquisitor/BedCheck.java
@@ -1,6 +1,6 @@
 package com.frdfsnlght.inquisitor;
 
-import static com.frdfsnlght.inquisitor.PlayerStats.group;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.UUID;
@@ -18,50 +18,49 @@ import org.bukkit.Server;
 public final class BedCheck implements Runnable {
 
     private final Server server;
-    private final Collection<UUID> bedOwners;
+    private final Collection<UUID> bedOwners;//Synchronized Set
+    private final ArrayList<UUID> bedOwnersSnapshot;
 
     /**
      *
      * @param server server to check
-     * @param bedOwners players to check
+     * @param bedOwners synchronized set of players to check
      */
     public BedCheck(Server server, Collection<UUID> bedOwners) {
         this.server = server;
         this.bedOwners = bedOwners;
+        synchronized (bedOwners) {
+            bedOwnersSnapshot = new ArrayList(bedOwners);
+        }
     }
 
     @Override
     public void run() {
-        Iterator<UUID> iterator = bedOwners.iterator();
-        while (iterator.hasNext()) {
-            UUID uuid = iterator.next();
-            OfflinePlayer player = server.getOfflinePlayer(uuid);
-            if (player == null) {//Needed?
-                player = server.getPlayer(uuid);
-                if (player == null) {
-                    continue;
-                }
-            }
-            if (!player.hasPlayedBefore()) {
+        for (UUID uuid : bedOwnersSnapshot) {
+            OfflinePlayer offlinePlayer = server.getOfflinePlayer(uuid);
+
+            if (!offlinePlayer.isOnline() && !offlinePlayer.hasPlayedBefore()) {
                 continue;
             }
-            if (player.getBedSpawnLocation() == null || player.getBedSpawnLocation().getBlock().getType() != Material.BED_BLOCK) {
-                iterator.remove();
 
-                final InquisitorPlayer player_ = new InquisitorPlayer.InquisitorPlayerImpl(player.getUniqueId(), player.getName());
+            if (offlinePlayer.getBedSpawnLocation() == null || offlinePlayer.getBedSpawnLocation().getBlock().getType() != Material.BED_BLOCK) {
+                bedOwners.remove(uuid);
+
+                final InquisitorPlayer inqPlayer = new InquisitorPlayer.InquisitorPlayerImpl(offlinePlayer.getUniqueId(), offlinePlayer.getName());
 
                 PlayerStats.submitChange(() -> {
-                    Utils.debug("player '%s' no longer has a bed", player_.getUUID());
-                    Statistics stats = group.getStatistics(player_);
+                    Utils.debug("player '%s' no longer has a bed", inqPlayer);
+                    Statistics stats = PlayerStats.group.getStatistics(inqPlayer);
                     stats.set(Statistic.bedServer, null);
                     stats.set(Statistic.bedWorld, null);
                     stats.set(Statistic.bedCoords, null);
                     stats.flush();
-                    if (!player_.isOnline()) {
-                        group.removeStatistics(stats);
+                    if (!inqPlayer.isOnline()) {
+                        PlayerStats.group.removeStatistics(stats);
                     }
                 });
             }
         }
+
     }
 }
